08878ec26942e49fc50dd019590dcf7544c5668d
Shu Takahashi <7193372+shutallbridge@users.noreply.github.com>
2024-05-18 11:06:00 +1000

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  extends: ["plugin:p5js/p5"],
+  plugins: ["p5js"],
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    "p5js/p5": true,
+  },
+};
