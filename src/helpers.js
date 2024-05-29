9c32aa344aac4017f282e1529b7d5549d6f4d1f5
Shu Takahashi <7193372+shutallbridge@users.noreply.github.com>
2024-05-29 16:35:16 +1000

    minor fix

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,18 @@
-const BREAK_POINTS = {
+/**
+ * These values define the breakpoints
+ */
+const BREAKPOINTS = {
   sm: 640,
   md: 768,
   lg: 1024,
 };
 
 function breakPointValue(object, width) {
-  if (BREAK_POINTS.lg < width) {
+  if (BREAKPOINTS.lg < width) {
     return object["lg"];
-  } else if (BREAK_POINTS.md < width) {
+  } else if (BREAKPOINTS.md < width) {
     return object["md"];
-  } else if (BREAK_POINTS.sm < width) {
+  } else if (BREAKPOINTS.sm < width) {
     return object["sm"];
   } else {
     return object["default"];
